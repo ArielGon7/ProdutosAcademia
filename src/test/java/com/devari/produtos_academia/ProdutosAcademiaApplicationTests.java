474ae9cadb271a56cd3625af784df228f08cb627
ArielGon7 <arielgomessantos7@gmail.com>
2026-04-21 19:08:09 -0300

    Primeiro commit

---
+++ src/test/java/com/devari/produtos_academia/ProdutosAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devari.produtos_academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
