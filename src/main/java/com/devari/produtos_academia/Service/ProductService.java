33f14d063d37e850b0c7d44c273df08a65470da3
ArielGon7 <arielgomessantos7@gmail.com>
2026-04-25 13:18:26 -0300

    Service implementada.

---
+++ src/main/java/com/devari/produtos_academia/Service/ProductService.java
@@ -0,0 +1,41 @@
+package com.devari.produtos_academia.Service;
+
+import com.devari.produtos_academia.Entity.ProductEntity;
+import com.devari.produtos_academia.Repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<ProductEntity> findAll(){
+        return productRepository.findAll();
+    }
+
+    public ProductEntity save(ProductEntity productEntity){
+        return productRepository.save(productEntity);
+    }
+
+    public void delete(Long id) {
+        if (!productRepository.existsById(id)){
+            throw new RuntimeException("id not found");
+        }
+        productRepository.deleteById(id);
+    }
+
+    public ProductEntity upload(Long id, ProductEntity productEntity){
+        ProductEntity productDb = productRepository.findById(id).orElseThrow(() -> new RuntimeException("id not found"));
+
+        productDb.setName(productEntity.getName());
+        productDb.setPrice(productEntity.getPrice());
+        productDb.setWeight(productEntity.getWeight());
+
+        return productRepository.save(productDb);
+
+    }
+
+}
